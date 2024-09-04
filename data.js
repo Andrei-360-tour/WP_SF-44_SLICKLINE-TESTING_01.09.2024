f46e12da6c99b2e101a80b2922a5de5a80ddf3c8
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-09-04 11:11:04 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0311",
+      "name": "DJI_0311",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0713241007563994,
+        "pitch": 0.6101264672474329,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5425900304090714,
+          "pitch": 0.6229269625892897,
+          "rotation": 0,
+          "target": "2-dji_0313"
+        },
+        {
+          "yaw": 0.4724586252802947,
+          "pitch": 0.07488704979907013,
+          "rotation": 0,
+          "target": "1-dji_0312"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dji_0312",
+      "name": "DJI_0312",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.230045747373941,
+        "pitch": 0.38201466808586915,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3711918111666321,
+          "pitch": 0.23705774169831706,
+          "rotation": 0,
+          "target": "0-dji_0311"
+        },
+        {
+          "yaw": 0.7613759194286729,
+          "pitch": 0.15837445323492716,
+          "rotation": 0,
+          "target": "2-dji_0313"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dji_0313",
+      "name": "DJI_0313",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.7959691013908721,
+        "pitch": 0.3299217588014294,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40700135535982085,
+          "pitch": 0.028738480461681704,
+          "rotation": 0,
+          "target": "0-dji_0311"
+        },
+        {
+          "yaw": 1.0970828703773687,
+          "pitch": -0.014121218580740447,
+          "rotation": 0,
+          "target": "1-dji_0312"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SF-44",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
